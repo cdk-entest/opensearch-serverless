c6150380cdc6057d14d9288ce0cf78899eef5d31
entest <hai@entest.io>
2022-12-10 01:03:14 +0000

    first

--- README.md
+++ README.md
@@ -1,14 +1,145 @@
-# Welcome to your CDK TypeScript project
+---
+title: opensearch serverless
+description: build a note bookmark using opensearch serverless
+author: haimtran
+publishedDate: 06/11/2022
+date: 06-11-2022
+---
 
-This is a blank project for CDK development with TypeScript.
+## Introduction
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+- setup aws opensearch
+- interact via curl
+- using python sdk
+- [basic dsl language](https://opensearch.org/docs/latest/opensearch/query-dsl/full-text/#match)
 
-## Useful commands
+## Architecture
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+![Untitled Diagram drawio](https://user-images.githubusercontent.com/20411077/205195719-e84c604d-c038-4aa2-9da0-27cd1f83cd11.png)
+
+## Configure Permission
+
+from opensearch aws console, need grant permission to user, role to access collection and index by polcies, example
+
+```json
+
+```
+
+## OpenSearch Client
+
+auth and create a client
+
+```py
+# opensearch domain
+if "OPENSEARCH_DOMAIN" in os.environ:
+    pass
+else:
+    os.environ["OPENSEARCH_DOMAIN"] = ""
+    os.environ["REGION"] = "us-east-1"
+
+# host and opensearch client
+host = os.environ["OPENSEARCH_DOMAIN"]
+client = boto3.client("opensearchserverless")
+service = "aoss"
+region = os.environ["REGION"]
+credentials = boto3.Session().get_credentials()
+
+# auth
+awsauth = AWS4Auth(
+    credentials.access_key,
+    credentials.secret_key,
+    region,
+    service,
+    session_token=credentials.token,
+)
+```
+
+create a client
+
+```py
+client = OpenSearch(
+    hosts=[{"host": host, "port": 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    timeout=300,
+)
+```
+
+create an index
+
+```py
+resp = client.indices.create(index_name)
+```
+
+delete an index
+
+```py
+resp = client.indices.delete(index=index_name)
+```
+
+index a document
+
+```py
+def test_index_data(index_name):
+    """
+    test index a document into the opensearch serverless
+    """
+    #
+    response = client.index(
+        index=index_name,
+        # body={"title": "pricing", "creator": "Larry David", "year": 1989},
+       body={
+            "DocumentTitle": "pricing",
+            "DocumentExcerpt": "100",
+            "DocumentURI":"URL"
+        },
+        id=str(uuid.uuid4()),
+    )
+    print(response)
+```
+
+query data
+
+```py
+def test_query_index(query):
+    """
+    test query from opensearch serverless
+    """
+    response = client.search(
+        index="cdk-entest", body={"query": {"query_string": {"query": query}}}
+    )
+    print(response)
+```
+
+or test another query
+
+```py
+def test_query(query_request):
+    """
+    query opensearch
+    """
+    resp = client.search(
+        index="cdk-entest",
+        body={
+            "query": {
+                "multi_match": {
+                    "query": query_request,
+                    "fields": [
+                        "DocumentTitle",
+                        "DocumentExcerpt",
+                        "DocumentURI",
+                    ],
+                }
+            }
+        },
+    )
+    print(resp)
+```
+
+delete a document given the document id
+
+```py
+client.delete(index=index_name, id=id)
+```
--- lib/opensearch-note-stack.ts
+++ lib/opensearch-note-stack.ts
@@ -1,16 +1,105 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
+
+interface OpensearchNoteProps extends StackProps {
+  opensearchDomain: string;
+}
 
 export class OpensearchNoteStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: OpensearchNoteProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // role for lambda to read opensearch
+    const role = new cdk.aws_iam.Role(this, "RoleForLambdaQueryOpenSearch", {
+      roleName: "RoleForLambdaQueryOpenSearch",
+      assumedBy: new cdk.aws_iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OpensearchNoteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // lambda function to query opensearch
+    const func = new cdk.aws_lambda.Function(this, "LamdaQueryOpenSearch", {
+      functionName: "LamdaQueryOpenSearch",
+      memorySize: 512,
+      timeout: Duration.seconds(10),
+      code: cdk.aws_lambda.EcrImageCode.fromAssetImage(
+        path.join(__dirname, "./../lambda")
+      ),
+      handler: cdk.aws_lambda.Handler.FROM_IMAGE,
+      runtime: cdk.aws_lambda.Runtime.FROM_IMAGE,
+      environment: {
+        OPENSEARCH_DOMAIN: props.opensearchDomain,
+        PYTHONPATH: "/var/task/package",
+        REGION: this.region,
+      },
+      role: role,
+    });
+
+    // api gateway
+    const apigw = new cdk.aws_apigateway.RestApi(this, "OpenSearchApi", {
+      restApiName: "opensearch",
+    });
+
+    const resource = apigw.root.addResource("cdk-entest");
+
+    resource.addMethod("GET", new cdk.aws_apigateway.LambdaIntegration(func));
+  }
+}
+
+interface DDBStreamProps extends StackProps {
+  openSearchDomain: string;
+  tableArn: string;
+  tableStreamArn: string;
+}
+
+export class DDBStreamStack extends Stack {
+  constructor(scope: Construct, id: string, props: DDBStreamProps) {
+    super(scope, id, props);
+
+    // role for lambda to read opensearch
+    const role = new cdk.aws_iam.Role(this, "RoleForLambdaIndexOpenSearch", {
+      roleName: "RoleForLambdaIndexOpenSearch",
+      assumedBy: new cdk.aws_iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    // lambda function
+    const func = new cdk.aws_lambda.Function(this, "LambdaIndexOpenSearch", {
+      functionName: "LambdaIndexOpenSearch",
+      memorySize: 512,
+      timeout: Duration.seconds(10),
+      code: cdk.aws_lambda.EcrImageCode.fromAssetImage(
+        path.join(__dirname, "./../lambda-index-oss")
+      ),
+      handler: cdk.aws_lambda.Handler.FROM_IMAGE,
+      runtime: cdk.aws_lambda.Runtime.FROM_IMAGE,
+      environment: {
+        OPENSEARCH_DOMAIN: props.openSearchDomain,
+        PYTHONPATH: "/var/task/package",
+        REGION: this.region,
+      },
+      role: role,
+    });
+
+    // existing ddb table
+    const table = cdk.aws_dynamodb.Table.fromTableAttributes(
+      this,
+      "NoteTableStream",
+      {
+        tableArn: props.tableArn,
+        tableStreamArn: props.tableStreamArn,
+      }
+    );
+
+    // configure ddb stream to trigger lambda
+    func.addEventSource(
+      new cdk.aws_lambda_event_sources.DynamoEventSource(table, {
+        startingPosition: cdk.aws_lambda.StartingPosition.LATEST,
+        batchSize: 5,
+        maxBatchingWindow: Duration.seconds(1),
+        bisectBatchOnError: true,
+        retryAttempts: 2,
+        enabled: true,
+      })
+    );
   }
 }
